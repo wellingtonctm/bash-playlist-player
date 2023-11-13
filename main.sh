9ff7669a7949c7390a631452243443eb4605ffbe
ext_wn8148 <wellington.marciel@grendene.com.br>
2023-11-13 10:10:46 -0300

    refactor(main.sh): adicionar diretorio de conf e pids

--- main.sh
+++ main.sh
@@ -2,8 +2,18 @@
 
 app_title='Bash Playlist Player'
 base_dir=$(dirname "$(readlink -f "$(which "$0")")")
+pids_dir="/var/run/user/$(id -u)/bash-playlist-player"
+conf_dir="$(getent passwd "$(logname)" | cut -d: -f6)/.config/bash-playlist-player"
 
-main_pid_file="$base_dir/main.pid"
+if [[ ! -d "$pids_dir" ]]; then
+    mkdir -p "$pids_dir"
+fi
+
+if [[ ! -d "$conf_dir" ]]; then
+    mkdir -p "$conf_dir"
+fi
+
+main_pid_file="$pids_dir/main.pid"
 
 if [[ -f $main_pid_file ]] && kill -0 $(cat $main_pid_file) &> /dev/null; then
     echo "There is an instance already running ($(cat $main_pid_file))."
@@ -12,11 +22,11 @@ fi
 
 echo $$ > $main_pid_file
 
-options_file="$base_dir/options.conf"
-song_info_file="$base_dir/song.info"
-song_pid_file="$base_dir/song.pid"
-keys_pid_file="$base_dir/keys.pid"
-playlists_dir="$base_dir/.playlists"
+options_file="$conf_dir/options.conf"
+song_info_file="$conf_dir/song.info"
+song_pid_file="$pids_dir/song.pid"
+keys_pid_file="$pids_dir/keys.pid"
+playlists_dir="$conf_dir/.playlists"
 
 mkdir -p "$playlists_dir"
 
